24fad6b36970488bd0476a7ab9641d06213e80b2
chyrt <geertevertperton@gmail.com>
2024-03-30 20:27:07 +0100

    Refactor database credentials to use environment variables

--- webapp/src/database.php
+++ webapp/src/database.php
@@ -1,5 +1,12 @@
 <?php
 
+include_once '../../config.php';
+
+$host = getenv("DB_HOST");
+$user = getenv("DB_USER");
+$pass = getenv("DB_PASS");
+$name = getenv("DB_NAME");
+
 function get_state() {
     return serialize([$_SESSION['hand'], $_SESSION['board'], $_SESSION['player']]);
 }
@@ -11,6 +18,12 @@ function set_state($state) {
     $_SESSION['player'] = $c;
 }
 
-return new mysqli('db', 'root', '', 'hive');
+$conn = new mysqli($host, $user, $pass, $name);
+
+// Controleer de connectie
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+return $conn;
 
 ?>
